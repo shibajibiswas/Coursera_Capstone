d02faf7e02c5a06118fdd9117f424417a8ab7557
shibaji <shibaji005@gmail.com>
2019-04-18 19:34:23 +0530

    Added a python script for greeting Capstone Project

---
+++ myPythonScript.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# ## This Notebook is mainly used for capstone project of IBM data science professional certification offered by Coursera
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
